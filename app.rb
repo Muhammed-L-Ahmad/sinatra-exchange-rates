0c8624645c226d645748fae421638179581d1d55
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-05-08 18:57:43 +0000

    changes committed

--- app.rb
+++ app.rb
@@ -1,9 +1,49 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
+
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+
+  @currencies = @parsed_response.fetch("currencies")
+
+  erb(:homepage)
+
+end
+
+get("/:from_currency") do
+
+  @the_symbol = params.fetch("first_symbol")
+  
+  api_url = HTTP.get"https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATES_KEY").chomp}"
+
+  @raw_response = HTTP.get(api_url)
+
+  @raw_string = @raw_response.to_s
+
+  @parsed_data = JSON.parse(@raw_string)
+
+  @currencies = @parsed_data.fetch("currencies")
+
+  erb(:step_one)
+
+end
+
+
+get("/:from_currency"/"to_currency") do
+  @from = params.fetch("from_currency")
+  @to = params.fetch("to_currency")
+
+  @converter_api_url = HTTP.get"https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATES_KEY").chomp}&from=#{@from}&to=#{@to}&amount=1"
+  @raw_response = HTTP.get(@converter_api_url)
+  @raw_string = @raw_response.to_s
+  @parsed_data = JSON.parse(@raw_string)
+  @amount = parsed_data.fetch("result")
+
+  erb(:step_two)
 end
