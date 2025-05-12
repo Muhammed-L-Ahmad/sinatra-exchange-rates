4a70c94747043db1f0fe717580953de954db65f8
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-05-12 15:55:21 +0000

    Fixed typos and added left out items

--- app.rb
+++ app.rb
@@ -42,11 +42,11 @@ get("/:from_currency/:to_currency") do
   @from = params.fetch("from_currency")
   @to = params.fetch("to_currency")
 
-  @converter_api_url = HTTP.get"https://api.exchangerate.host/convert?access_key=#{EXCHANGE_RATES_KEY}&from=#{@from}&to=#{@to}&amount=1"
+  @converter_api_url = "https://api.exchangerate.host/convert?access_key=#{EXCHANGE_RATES_KEY}&from=#{@from}&to=#{@to}&amount=1"
   @raw_response = HTTP.get(@converter_api_url)
   @raw_string = @raw_response.to_s
   @parsed_data = JSON.parse(@raw_string)
-  @amount = parsed_data.fetch("result")
+  @amount = @parsed_data.fetch("result")
 
   erb(:step_two)
 end
